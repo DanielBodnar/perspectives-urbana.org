8649c35387fcec4609ec90d2c486f4499829fca3
Daniel Bodnar <daniel.bodnar@gmail.com>
2015-09-18 14:19:10 -0500

    bunch of tweaks

---
+++ js/app.js
@@ -0,0 +1,34 @@
+angular.module('app', [
+    'ui.router',
+    'ngResource',
+    //'lbServices',
+    //'truncate',
+    'ngCookies'
+])
+
+.config(['$urlRouterProvider', function($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+}])
+
+.config(function($httpProvider) {
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+
+.run(function($rootScope, $state, Settings) {
+    $rootScope.settings = Settings;
+});
+
+
+angular.module('app')
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                controller: 'homeCtrl',
+                controllerAs: 'home',
+                url: '/home',
+                templateUrl: '/app/home/index.html'
+            });
+    }])
+
+.controller('homeCtrl', function($scope, $state) {
+})
---
+++ js/directives/directives.js
@@ -0,0 +1,153 @@
+angular.module('app')
+
+.directive('init', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, element, attrs) {
+            init();
+        }
+    }
+})
+
+.directive('autoheight', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+
+            var setElementHeight = function() {
+                var height = angular.element(window).height();
+                element.css('min-height', (height));
+            }
+            angular.element(window).on("resize", function() {
+                setElementHeight();
+            }).resize();
+
+            setElementHeight();
+        }
+    }
+})
+
+.directive('jssor', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, element, attrs) {
+            var bodyWidth = document.body.clientWidth;
+            if (bodyWidth)
+                element.$SetScaleWidth(Math.min(bodyWidth, 980));
+            else
+                window.setTimeout(ScaleSlider, 30);
+
+
+            if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+                $(window).bind('resize', ScaleSlider);
+            }
+        }
+    }
+})
+
+.directive('smoothScroll', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, element, attrs) {
+
+            var height = element.height();
+            var scroll = angular.element(window).scrollTop();
+            if (scroll > height) {
+                angular.element(".header-hide").addClass("scroll-header");
+            }
+
+            smoothScroll.init({
+                speed: 1000,
+                easing: 'easeInOutCubic',
+                offset: height,
+                updateURL: false
+            });
+
+            angular.element(window).scroll(function() {
+                var height = angular.element(window).height();
+                var scroll = angular.element(window).scrollTop();
+                if (scroll) {
+                    angular.element(".header-hide").addClass("scroll-header");
+                } else {
+                    angular.element(".header-hide").removeClass("scroll-header");
+                }
+
+            });
+        }
+    }
+})
+
+
+.directive('venobox', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+            element.venobox({
+                numeratio: true,
+                infinigall: true,
+                border: '20px'
+            })
+        }
+    }
+})
+
+.directive('venoboxvid', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+            element.venobox({
+                bgcolor: '#000'
+            })
+        }
+    }
+})
+
+.directive('venoboxframe', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+            element.venobox({
+                border: '6px'
+            })
+        }
+    }
+})
+
+.directive('venoboxinline', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+            element.venobox({
+                framewidth: '300px',
+                frameheight: '250px',
+                border: '6px',
+                bgcolor: '#f46f00'
+            })
+        }
+    }
+})
+
+.directive('venoboxajax', function() {
+    return {
+        restrict: 'C',
+        link: function($scope, element, attrs) {
+            element.venobox({
+                border: '30px;',
+                frameheight: '220px'
+            })
+        }
+    }
+})
+
+
+jQuery('#defaultCountdown').countdown({
+    until: new Date(2016, 1, 12)
+});
+// .directive('init', function() {
+//     return {
+//         restrict: 'A',
+//         link: function($scope, element, attrs) {
+//             init();
+//         }
+//     }
+// })
